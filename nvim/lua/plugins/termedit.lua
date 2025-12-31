bd500c9b3759d8b58f3c3703ccab49fc78d74a5d
xbilek26 <bilekf@email.cz>
2025-12-31 18:09:50 +0100

    Edit dotfiles

--- nvim/lua/plugins/termedit.lua
+++ nvim/lua/plugins/termedit.lua
@@ -3,16 +3,8 @@ return {
     event = "TermOpen",
     version = "1.*",
     config = function()
-        require 'term-edit'.setup {
-            -- Mandatory option:
-            -- Set this to a lua pattern that would match the end of your prompt.
-            -- Or a table of multiple lua patterns where at least one would match the
-            -- end of your prompt at any given time.
-            -- For most bash/zsh user this is '%$ '.
-            -- For most powershell/fish user this is '> '.
-            -- For most windows cmd user this is '>'.
+        require("term-edit").setup({
             prompt_end = '%$ ',
-            -- How to write lua patterns: https://www.lua.org/pil/20.2.html
-        }
+        })
     end,
 }
