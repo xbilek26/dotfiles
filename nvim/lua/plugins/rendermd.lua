14670ffefaff6e75df1f3c6acb68fe10ad523556
xbilek26 <bilekf@email.cz>
2025-12-31 19:51:26 +0100

    Edit dotfiles

--- nvim/lua/plugins/rendermd.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "MeanderingProgrammer/render-markdown.nvim",
-    requires = { "nvim-tree/nvim-web-devicons", opt = true },
-    config = function()
-        require("render-markdown").setup({})
-    end,
-}
