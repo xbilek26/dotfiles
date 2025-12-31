4cb2d476c0878a2ac037dc8be97a0b4d6860af58
xbilek26 <bilekf@email.cz>
2025-12-31 18:49:21 +0100

    Edit dotfiles

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -7,22 +7,6 @@ return {
             options = {
                 theme = "gruvbox",
             },
-            sections = {
-                lualine_a = {'mode'},
-                lualine_b = {'branch'},
-                lualine_c = {'filename'},
-                lualine_x = {'encoding', 'fileformat', 'filetype'},
-                lualine_y = {'progress'},
-                lualine_z = {'location'}
-            },
-            inactive_sections = {
-                lualine_a = {'filename'},
-                lualine_b = {'branch'},
-                lualine_c = {},
-                lualine_x = {'encoding', 'fileformat', 'filetype'},
-                lualine_y = {'progress'},
-                lualine_z = {'location'}
-            },
         })
     end,
 }
