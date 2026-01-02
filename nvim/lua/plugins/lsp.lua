f97fac4e490d39197206ebf1d3d0df536aeaeeda
xbilek26 <bilekf@email.cz>
2026-01-02 21:39:37 +0100

    Edit dotfiles

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,11 +4,27 @@ return {
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, { silent = true })
         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { silent = true })
 
-        vim.lsp.config["clangd"] = {
-            cmd = { "clangd" },
-            filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-            root_markers = { "compile_commands.json", "compile_flags.txt", ".git" },
-        }
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "c", "cpp" },
+            callback = function()
+                vim.lsp.start({
+                    cmd = { "clangd" },
+                    filetypes = { "c", "cpp" },
+                    root_markers = { "compile_commands.json", ".git" },
+                })
+            end,
+        })
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "java" },
+            callback = function()
+                vim.lsp.start({
+                    cmd = { "jdtls" },
+                    filetypes = { "java" },
+                    root_markers = { "gradlew", "mvnw", ".git" },
+                })
+            end,
+        })
 
         vim.diagnostic.config({
             virtual_text = true,
@@ -16,16 +32,6 @@ return {
             underline = true,
             update_in_insert = false,
         })
-
-        vim.api.nvim_create_autocmd(
-            "FileType",
-            {
-                pattern = {"c", "cpp"},
-                callback = function()
-                    vim.lsp.start(vim.lsp.config["clangd"])
-                end
-            }
-        )
 
     end,
 }
