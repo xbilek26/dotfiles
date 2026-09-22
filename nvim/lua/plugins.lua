876fe6c2552fc98de2d4f30774216cf14dadf39b
xbilek26 <bilekf@email.cz>
2026-09-23 00:52:37 +0200

    Edit dotfiles

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -8,7 +8,6 @@ require("vague").setup({
     on_highlights = function(hl, colors)
         hl.ModeMsg = { fg = colors.fg }
         hl.NetrwMarkFile = { bold = true }
-        hl.Pmenu = { bg = nil }
         hl.Directory = { fg = "#8ba9c1" }
     end,
 })
@@ -29,10 +28,26 @@ vim.pack.add({
     "https://github.com/nvim-treesitter/nvim-treesitter",
 })
 
+require("nvim-treesitter").install({
+    "java",
+    "python",
+    "cpp",
+    "javascript",
+    "typescript"
+})
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "java" },
-    callback = function() vim.treesitter.start() end,
+    pattern = {
+        "java",
+        "python",
+        "c",
+        "cpp",
+        "javascript",
+        "typescript"
+    },
+    callback = function(args)
+        pcall(vim.treesitter.start, args.buf)
+    end,
 })
 
 ------------------------------------------------------------
@@ -52,8 +67,10 @@ vim.pack.add({
 })
 
 vim.lsp.enable({
-    "clangd",
     "basedpyright",
+    "clangd",
+    "jdtls",
+    "ts_ls"
 })
 
 vim.diagnostic.config({
@@ -64,3 +81,17 @@ vim.diagnostic.config({
 })
 
 vim.diagnostic.status = function() return "" end
+
+------------------------------------------------------------
+
+vim.pack.add({
+    "https://github.com/nvim-lua/plenary.nvim",
+    "https://github.com/nvim-telescope/telescope.nvim",
+})
+
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files)
+vim.keymap.set('n', '<leader>fo', builtin.oldfiles)
+vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+vim.keymap.set('n', '<leader>fb', builtin.buffers)
